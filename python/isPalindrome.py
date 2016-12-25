ec5693b3e827891360b6c827fe38e1a212f8db9b
Gaurav Dalvi <gaurav1424@gmail.com>
2016-12-25 11:52:29 -0800

    Update isPalindrome.py

--- python/isPalindrome.py
+++ python/isPalindrome.py
@@ -16,6 +16,14 @@
 # test => no
 # a => yes
 
+# def isPalindromeRecursive(string):
+#     if string == '' or len(string) == 1:
+#         return True
+#     if string[0] == string[-1]:
+#         return isPalindromeRecursive(string[1:len(string) - 1])
+#
+#     return False
+
 def isPalindrome(string):
 
     punctuation = ['.', ',', '!', '-', ';', ':', '\\', '"', ' ', '\n']
