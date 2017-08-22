e5f373b00d7b91897d45350e4ce6792902e53cb3
Gaurav Dalvi <gaurav1424@gmail.com>
2017-08-21 23:05:03 -0700

    Update binarySearch.py

--- python/binarySearch.py
+++ python/binarySearch.py
@@ -1,19 +1,33 @@
 # Implement Binary search.
 # Key Learnings : mid -1 and mid+1 in recursive call
 
-def binarySearch(nums, low, high, key):
+def binary_search_recursive(nums, low, high, key):
 
     mid = (high + low) / 2
     if nums[mid] == key:
         return True
     if low < high:
         if nums[mid] > key:
-            return binarySearch(nums, low, mid-1, key)
+            return binary_search_recursive(nums, low, mid-1, key)
         else:
-            return binarySearch(nums, mid+1, high, key)
+            return binary_search_recursive(nums, mid+1, high, key)
+    return False
+
+def binary_search_iterative(nums, low, high, key):
+
+    while low <= high:
+        mid = (low + high) / 2
+        if nums[mid] == key:
+            return True
+        else:
+            if nums[mid] > key:
+                high = mid - 1
+            else:
+                low = mid + 1
+
     return False
 
 
 if __name__ == '__main__':
     nums = [1,2,3,4,5,6]
-    print binarySearch(nums, 0, len(nums)-1, 3)
+    print binary_search_iterative(nums, 0, len(nums)-1, 8)
