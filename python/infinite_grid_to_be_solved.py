b089aeb4c54e3dc7015e9446de0a3058e9e68afe
Gaurav Dalvi <gaurav1424@gmail.com>
2017-10-07 22:23:44 -0700

    Create infinite_grid_to_be_solved.py

---
+++ python/infinite_grid_to_be_solved.py
@@ -0,0 +1,5 @@
+Julie starts cell in infinite matrix from bottom left. Each cell is zero. The game consist of n steps.
+During each step:
+1: two int r ,c
+2: Increments value of each cell such that : 1 <= i <= r and 1 <= j <= c
+The fuction must return longer integer denoting total number of occurences of greates integer of grid after n steps.
