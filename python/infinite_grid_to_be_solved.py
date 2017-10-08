74799e6bbe5fa9d134181b8664d41c8805fb19fb
Gaurav Dalvi <gaurav1424@gmail.com>
2017-10-07 22:24:48 -0700

    Update infinite_grid_to_be_solved.py

--- python/infinite_grid_to_be_solved.py
+++ python/infinite_grid_to_be_solved.py
@@ -3,3 +3,6 @@ During each step:
 1: two int r ,c
 2: Increments value of each cell such that : 1 <= i <= r and 1 <= j <= c
 The fuction must return longer integer denoting total number of occurences of greates integer of grid after n steps.
+
+Input: [(2,3),(3,7), (4,1)]
+Output: 2
