31c345cbfc9fbd5df178553249bd4f12614e2919
Gaurav Dalvi <gaurav1424@gmail.com>
2020-06-07 13:03:04 -0700

    Update edit_distance_leetcode_72.py

--- python/edit_distance_leetcode_72.py
+++ python/edit_distance_leetcode_72.py
@@ -8,7 +8,7 @@ def edit_distance_recursive(source, destination, m, n):
 		return n
 	if n == 0:
 		return m
-	if source[m] == destination[n]:
+	if source[m-1] == destination[n-1]:
 		return edit_distance_recursive(source, destination, m-1, n-1)
 	else:
 		delete_count = edit_distance_recursive(source, destination, m-1, n)
@@ -16,16 +16,35 @@ def edit_distance_recursive(source, destination, m, n):
 		insert_count = edit_distance_recursive(source, destination, m, n-1)
 		return 1 + min(delete_count, replace_count, insert_count)
 
-# source='rosrose'
-# destination='rose'
-# print(edit_distance_recursive(source, destination, len(source)-1, len(destination)-1))
+def edit_distance_dp(source, destination, m, n):
+	dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+	for i in range(m+1):
+		for j in range(n+1):
+			if i == 0:
+				dp[i][j] = j
+			elif j == 0:
+				dp[i][j] = i
+			elif source[i-1] == destination[j-1]:
+				dp[i][j] = dp[i-1][j-1]
+			else:
+				dp[i][j] = 1 + min(dp[i-1][j], # delete
+                                   dp[i-1][j-1], # replace
+                                   dp[i][j-1]) # insert
+	return dp[m][n]
 
-# source='sunday'
-# destination='saturday'
-# print(edit_distance_recursive(source, destination, len(source)-1, len(destination)-1))
+source='rosrose'
+destination='rose'
+print(edit_distance_recursive(source, destination, len(source), len(destination)))
+print(edit_distance_dp(source, destination, len(source), len(destination)))
+
+source='sunday'
+destination='saturday'
+print(edit_distance_recursive(source, destination, len(source), len(destination)))
+print(edit_distance_dp(source, destination, len(source), len(destination)))
 
 source='geek'
 destination='gesek'
-print(edit_distance_recursive(source, destination, len(source)-1, len(destination)-1))
+print(edit_distance_recursive(source, destination, len(source), len(destination)))
+print(edit_distance_dp(source, destination, len(source), len(destination)))
 
 
